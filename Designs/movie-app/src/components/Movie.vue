1fdc33899dc02dbe040a7c4fb0e0baf6345b8534
Daniel Jimenez <danieljimenezch@Daniels-MacBook-Pro.local>
2023-03-10 15:31:36 -0600

    fix style in detail view

--- Designs/movie-app/src/components/Movie.vue
+++ Designs/movie-app/src/components/Movie.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-    <h2>{{ movie.title }}</h2>
-    <p>{{ movie.overview }}</p>
-
-    <router-link to='/'>Return</router-link>
+  <div class="card">
+    <img :src="movie.poster" :alt="movie.title" class="img-poster" />
+    <div class="card-body">
+      <h2 class="card-title">{{ movie.title }}</h2>
+      <p class="card-text">{{ movie.overview }}</p>
+      <p class="card-text"><strong>Rating:</strong> {{ movie.rating }}/10</p>
+      <p class="card-text"><strong>Length:</strong> {{ movie.length }} minutes</p>
+      <router-link to="/" class="btn btn-primary">Return</router-link>
+    </div>
   </div>
 </template>
+
+
 
 <script>
 import axios from 'axios';
@@ -33,3 +39,10 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ .img-poster {
+  width: 200px;
+  height: 350px;
+ }
+</style>>
